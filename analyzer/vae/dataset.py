6fee3273edbb990d14cee896bdce0d39ff0dc392
sidakk95 <kevin.sidak@univie.ac.at>
2021-02-10 00:49:39 +0100

    better size check

--- analyzer/vae/dataset.py
+++ analyzer/vae/dataset.py
@@ -46,44 +46,6 @@ class MitoDataset:
         with h5py.File(self.mito_volume_file_name, 'r') as f:
             return f[self.mito_volume_dataset_name][idx]
 
-    def get_mito_volume(self, region):
-        '''
-        Preprocessing function to extract and scale the mitochondria as volume
-        :param region: (dict) one region object provided by Dataloader.prep_data_info
-        :returns result: (numpy.array) a numpy array with the target dimensions and the mitochondria in it
-        '''
-        all_fn = sorted(glob.glob(self.gt_path + '*.' + self.ff))
-        target = tio.ScalarImage(tensor=torch.rand(self.target_size))
-        fns = [all_fn[id] for id in region['slices']]
-        first_image_slice = imageio.imread(fns[0])
-        mask = np.zeros(shape=first_image_slice.shape, dtype=np.uint16)
-        mask[first_image_slice == region['id']] = 1
-        volume = mask
-
-        for fn in fns[1:]:
-            image_slice = imageio.imread(fn)
-            mask = np.zeros(shape=image_slice.shape, dtype=np.uint16)
-            mask[image_slice == region['id']] = 1
-            volume = np.dstack((volume, mask))
-        volume = np.moveaxis(volume, -1, 0)
-
-        mito_regions = regionprops(volume, cache=False)
-        if len(mito_regions) != 1:
-            print("something went wrong during volume building. region count: {}".format(len(mito_regions)))
-
-        mito_region = mito_regions[0]
-        mito_volume = volume[mito_region.bbox[0]:mito_region.bbox[3] + 1,
-                      mito_region.bbox[1]:mito_region.bbox[4] + 1,
-                      mito_region.bbox[2]:mito_region.bbox[5] + 1].astype(np.float32)
-        mito_volume = np.expand_dims(mito_volume, 0)
-        if self.lower_limit > mito_volume.sum() > self.upper_limit:
-            return None
-
-        transform = tio.Resample(target=target, image_interpolation='nearest')
-        transformed_mito = transform(mito_volume)
-
-        return transformed_mito
-
     def extract_scale_mitos(self,
                             cpus=multiprocessing.cpu_count()):
         dl = analyzer.data.Dataloader(gtpath=self.gt_path, volpath=self.em_path)
@@ -120,3 +82,43 @@ class MitoDataset:
                             dset[i + j] = result
                             mito_counter += 1
                         print("{} mitos of {}".format(mito_counter, len(regions)))
+
+    def get_mito_volume(self, region):
+        '''
+        Preprocessing function to extract and scale the mitochondria as volume
+        :param region: (dict) one region object provided by Dataloader.prep_data_info
+        :returns result: (numpy.array) a numpy array with the target dimensions and the mitochondria in it
+        '''
+        all_fn = sorted(glob.glob(self.gt_path + '*.' + self.ff))
+        target = tio.ScalarImage(tensor=torch.rand(self.target_size))
+        fns = [all_fn[id] for id in region['slices']]
+        first_image_slice = imageio.imread(fns[0])
+        mask = np.zeros(shape=first_image_slice.shape, dtype=np.uint16)
+        mask[first_image_slice == region['id']] = 1
+        volume = mask
+
+        for fn in fns[1:]:
+            image_slice = imageio.imread(fn)
+            mask = np.zeros(shape=image_slice.shape, dtype=np.uint16)
+            mask[image_slice == region['id']] = 1
+            volume = np.dstack((volume, mask))
+        volume = np.moveaxis(volume, -1, 0)
+
+        mito_regions = regionprops(volume, cache=False)
+        if len(mito_regions) != 1:
+            print("something went wrong during volume building. region count: {}".format(len(mito_regions)))
+
+        mito_region = mito_regions[0]
+        if self.lower_limit > mito_region.area > self.upper_limit:
+            return None
+
+        mito_volume = volume[mito_region.bbox[0]:mito_region.bbox[3] + 1,
+                      mito_region.bbox[1]:mito_region.bbox[4] + 1,
+                      mito_region.bbox[2]:mito_region.bbox[5] + 1].astype(np.float32)
+        mito_volume = np.expand_dims(mito_volume, 0)
+
+
+        transform = tio.Resample(target=target, image_interpolation='nearest')
+        transformed_mito = transform(mito_volume)
+
+        return transformed_mito
