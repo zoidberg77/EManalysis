ce1c200d1abfc9db0cffaac5d41c01d1d250a3fa
frommwonderland <daniel.illner@outlook.de>
2021-05-04 16:42:35 +0200

    add ptcshapef to model

--- analyzer/model/build_model.py
+++ analyzer/model/build_model.py
@@ -89,6 +89,8 @@ class Clustermodel():
 					feat = self.fe.compute_seg_dist()
 				elif fns == 'shapef':
 					feat = self.fe.compute_vae_shape()
+				elif fns == 'ptc_shapef':
+					feat = self.fe.compute_vae_ptc_shape()
 				elif fns == 'texturef':
 					feat = self.fe.compute_vae_texture()
 				elif fns == 'circf':
