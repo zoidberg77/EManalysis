d013697bd56cc89893c208acaeb7b7bcea845239
sidakk95 <kevin.sidak@univie.ac.at>
2021-05-17 17:30:05 +0200

    added reconstruction to inference

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-#SBATCH --cpus-per-task 16 # number of nodes
-#SBATCH --mem 64g # memory pool for all cores
-#SBATCH -o emanalysis.out # STDOUT
-#SBATCH -e emanalysis.out # STDERR
-#SBATCH --gres=gpu:1
-/home/sidakk95cs/.conda/envs/EManalysis/bin/python main.py --cfg configs/mouseA2.yaml --mode ptcprep
