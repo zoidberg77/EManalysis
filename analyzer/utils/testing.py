90c236747167d8ca5d6236ba8eb70dbe2c1fa9a7
frommwonderland <daniel.illner@outlook.de>
2021-05-18 12:34:58 +0200

    small testing file

---
+++ analyzer/utils/testing.py
@@ -0,0 +1,21 @@
+import os, sys
+import h5py
+import numpy as np
+
+with h5py.File(os.path.join('datasets/vae/mouseA/pts.h5'), 'r') as h5f:
+    ptcs_dict = h5f.get('ptcs')
+
+    int_keys = np.array([int(x) for x in ptcs_dict.keys()])
+    labels = np.array(h5f["labels"][:])
+    print(ptcs_dict)
+    print(int_keys)
+    print(np.array(h5f["labels"][:]))
+
+    for idx in range(labels.shape[0]):
+        elem = labels[idx]
+        if elem in int_keys:
+            if idx % 100 == 0:
+                print('no errors yet.')
+            continue
+        else:
+            raiseValueError('Error')
