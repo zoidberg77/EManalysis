62c09a213f5dcf5f2d7e841b138c2f66f39e6b7e
sidakk95 <kevin.sidak@univie.ac.at>
2021-05-20 11:39:10 +0200

    added blue noise sampling

--- analyzer/data/ptc_dataset.py
+++ analyzer/data/ptc_dataset.py
@@ -73,7 +73,7 @@ class PtcDataset():
                     for key, points in tqdm(group.items(), total=len(group.keys())):
                         idxs = []
                         cloud = np.array(points)
-                        dists = pairwise_distances(points)
+                        dists = pairwise_distances(points, n_jobs=cfg.SYSTEM.NUM_CPUS)
                         possible_idx = list(np.arange(0, len(points)))
                         start = random.sample(possible_idx, 1)[0]
                         possible_idx.pop(start)
