9322e3b67e3078229496d35224a21ff4aa60b65a
sidakk95 <kevin.sidak@univie.ac.at>
2021-09-07 18:29:06 +0200

    fixing preprocessing

--- analyzer/data/dataset.py
+++ analyzer/data/dataset.py
@@ -414,7 +414,7 @@ class Dataloader():
         Function to extract the objects as volumes and scale them. Then its saves the scaled volumes to an h5 file.
         '''
         regions = self.prep_data_info(save=True)
-        regions = pd.DataFrame(regions).values.tolist()[:100]
+        regions = pd.DataFrame(regions).values.tolist()
 
         print("{} objects found in the ground truth".format(len(regions)))
 
