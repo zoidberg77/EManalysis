30b6e13c44e099000a4e2e8085a692cb0066051c
sidakk95 <kevin.sidak@univie.ac.at>
2021-09-22 18:51:07 +0200

    added logging to vae

--- analyzer/vae/model/vae.py
+++ analyzer/vae/model/vae.py
@@ -72,7 +72,7 @@ class Vae(pl.LightningModule):
 
         # encoding path
         self.down_layers = nn.ModuleList()
-        self.out_layer = torch.nn.Sigmoid
+        self.out_layer = torch.sigmoid
 
         for i in range(self.depth):
             kernel_size, padding = self._get_kernel_size(isotropy[i])
